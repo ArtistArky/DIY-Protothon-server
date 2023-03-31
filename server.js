0be63650b1da94fd7f5ac377c705d963416f3d80
mohithworks <mohithworks2002@gmail.com>
2023-03-31 18:09:21 +0530

    1st commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+const env = require("dotenv").config({ path: "./.env" });
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-08-01",
+});
+
+app.get("/", (res) => {
+  res.sendFile("Hello World");
+});
+
+app.post("/create-contribution", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "INR",
+      amount: 500000,
+      automatic_payment_methods: { enabled: true },
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
+
+app.listen(4000, () =>
+  console.log(`Server listening at http://localhost:4000`)
+);
